ae94c8e12749c82ecf8ff2885662ed5e004b63db
yuting.zhou <yuting.zhou@bianlifeng.com>
2017-09-01 15:01:59 +0800

    Remove redundancy

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "space"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    "extends": "airbnb-base"
 };
